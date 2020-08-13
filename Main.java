663c7b99df90ff12d74f832e89b64880cdec1f82
KAAPLL <69614761+KAAPLL@users.noreply.github.com>
2020-08-12 20:39:35 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		System.out.println("My name is Kapil Malladi.");
+		System.out.println("I plan to major in computer science.");
+		System.out.println("One of my hobbies is playing the cello.");
+		System.out.println("My favorite video game is Counter Strike: Global Offensive.");
+		System.out.println("Memes are love. Memes are life.");
+	}
+}
